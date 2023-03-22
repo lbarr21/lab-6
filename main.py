59c9d46ee9147ca45e638bc5e2b16569309a298f
Sierra Rodriguez <Cecil@Sierras-MacBook-Pro.local>
2023-03-22 18:37:00 -0400

    partner changes

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 x = True
-encode = []
 encoded_stng = ''
+
+def encode(password):
+    encode = []
+    for i in password:
+        i = int(i)
+        if i >= 7:
+            encode.append(str(int(i) - 7))
+        else:
+            encode.append(str(int(i) + 3))
+
+    return ''.join(encode)
+
+
+def decode(original):
+    decode = []
+    res = [int(ele) for ele in original]
+    for i in res:
+        if i == 0:
+            decode.append(7)
+        if i == 1:
+            decode.append(8)
+        if i == 2:
+            decode.append(9)
+        if i == 3:
+            decode.append((0))
+        if i > 3:
+            decode.append((int(i)-3))
+    return decode
+
+
+
+name = 0
+value = 0
 while x == True:
-    print('Menu\n-------------\n 1. Encode\n2. Decode\n3. Quit\n')
+    print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
     option = int(input('Please enter an option: '))
     if option == 1:
-        encode = []
         value = input('Please enter your password to encode: ')
-        for i in value:
-            encode.append(str(int(i) + 3))
-        encoded_stng = ''.join(encode)
         print('Your password has been encoded and stored!')
-        print(encoded_stng)
+        name = (encode(value))
 
     elif option == 2:
-        decode = 0  # change the 0 later pls
-        print(f"The encoded password is {decode}, and the original password is {encoded_stng}.")
+        value2 = decode(name)
+        print(f"The encoded password is {name}, and the original password is {value}.")
     elif option == 3:
         x = False
         break
